42bdf499330985fa81955000c1982b04d7161705
Kent Oyer <kent.oyer@gmail.com>
2014-07-28 21:21:11 -0400

    inital commit

--- mypdo.php
+++ mypdo.php
@@ -20,6 +20,52 @@ class MyPDO extends PDO{
 		return $array[2];
 	}
 
+	public function update($table,$data,$where) {
+	
+		$args = array();
+		
+		$query = "UPDATE `$table` SET ";
+		$con = '';
+		foreach ( $data as $key => $value ) {
+			$query .= "$con$key=?";
+			$con = ',';
+			$args[] = $value;
+		}
+		
+		$query .= " WHERE ";
+		$con = '';
+		foreach ( $where as $key => $value ) {
+			$query .= "$con$key = ?";
+			$con = " AND ";
+			$args[] = $value;
+		}
+
+        $reponse = parent::prepare($query);
+        $reponse->execute($args);
+        return $reponse;
+		
+	}
+
+	public function insert($table,$data) {
+	
+		$args = array();
+		$values = '';
+		$query = "INSERT INTO `$table` (";
+		$con = '';
+		foreach ( $data as $key => $value ) {
+			$query .= "$con$key";
+			$values .= "$con?";
+			$con = ',';
+			$args[] = $value;
+		}
+		$query .= ") VALUES ($values)";
+		
+        $reponse = parent::prepare($query);
+        $reponse->execute($args);
+        return $reponse;
+
+	}
+	
 }
 
 
